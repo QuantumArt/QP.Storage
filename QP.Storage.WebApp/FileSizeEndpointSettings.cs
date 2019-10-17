efb4c36079ffe1555b995b2e2071b6198b47e2d6
Paul Grigorenko <grigorenko.paul@gmail.com>
2019-10-17 15:10:42 +0300

    add file size endpoint

---
+++ QP.Storage.WebApp/FileSizeEndpointSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace QP.Storage.WebApp
+{
+    public class FileSizeEndpointSettings
+    {
+        /// <summary>
+        /// Базовый путь до endpoint, возвращащей размер файлов
+        /// </summary>
+        public string BasePath { get; set; } = "/_filesize";
+    }
+}
---
+++ QP.Storage.WebApp/FileSizeMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace QP.Storage.WebApp
+{
+    public class FileSizeMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IFileProvider _fileProvider;
+        private readonly FileSizeEndpointSettings _settings;
+
+        public FileSizeMiddleware(RequestDelegate next, IWebHostEnvironment hostingEnv, IOptions<FileSizeEndpointSettings> settings)
+        {
+            _next = next;
+            _fileProvider = hostingEnv.WebRootFileProvider;
+            _settings = settings.Value;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var basePath = _settings.BasePath.StartsWith("/") ?
+                _settings.BasePath :
+                $"/{_settings.BasePath}";
+
+            if (context.Request.Path.StartsWithSegments(basePath, out PathString subPath))
+            {
+                var fileInfo = _fileProvider.GetFileInfo(subPath.Value);
+                if (fileInfo.Exists)
+                {
+                    await context.Response.WriteAsync(fileInfo.Length.ToString());
+                }
+                else
+                {
+                    context.Response.StatusCode = 404;
+                }
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
