750a7767880b365b2dfde6e21a9cabb97986f68a
Pavel Celut <CelutP@gmail.com>
2023-11-21 21:49:47 +0300

    Change NLog version

--- QP.Storage.WebApp/Middleware/ReduceSettingsMiddleware.cs
+++ QP.Storage.WebApp/Middleware/ReduceSettingsMiddleware.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
+using NLog;
 using QP.Storage.WebApp.Settings;
 
 namespace QP.Storage.WebApp.Middleware;
@@ -10,6 +11,7 @@ public class ReduceSettingsMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly ImageResizeSettings _settings;
+    private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
 
     public ReduceSettingsMiddleware(RequestDelegate next, IOptions<ImageResizeSettings> settings)
     {
@@ -30,7 +32,9 @@ public class ReduceSettingsMiddleware
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 WriteIndented = true
             };
+            _logger.Log(LogLevel.Debug, $"Call to {_settings.BasePath}");
             await context.Response.WriteAsync(JsonSerializer.Serialize(_settings, serializeOptions));
+            
             return;
         }
 
