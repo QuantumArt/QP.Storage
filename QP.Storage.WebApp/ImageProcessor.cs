ae08e0a9843cedd3ee4995c2748bfe8c8c153d83
Pavel Celut <CelutP@gmail.com>
2023-11-21 17:16:59 +0300

    Refactoring #2

--- QP.Storage.WebApp/ImageProcessor.cs
+++ QP.Storage.WebApp/ImageProcessor.cs
@@ -1,16 +1,17 @@
-﻿using SixLabors.ImageSharp;
+﻿using System.Threading.Tasks;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 
 namespace QP.Storage.WebApp;
 
 public class ImageProcessor
 {
-    public void ResizeImage(string path, decimal size, string resizedImage)
+    public async Task ResizeImage(string path, decimal size, string resizedImage)
     {
-        using (var image = Image.Load(path))
-        {
-            image.Mutate(x => x.Resize((int)(image.Width / size), (int)(image.Height / size)));
-            image.Save(resizedImage);
-        }
+        using var image = await Image.LoadAsync(path);
+        var width = image.Width;
+        var height = image.Height;
+        image.Mutate(x => x.Resize((int)(width / size), (int)(height / size)));
+        await image.SaveAsync(resizedImage);
     }
 }
